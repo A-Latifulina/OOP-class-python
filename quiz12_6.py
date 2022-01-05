e8a6150ab8819b0ea4b58c7c6465fcef78034f56
Adelya <adelya@astar.app>
2022-01-05 18:31:22 -0500

    Add files via upload

---
+++ quiz12_6.py
@@ -0,0 +1,14 @@
+class Car:
+    def __init__(self, year, model)
+        self.speed() = 0
+        self.year = year
+        self.model= model
+
+    def set_accelerate(self)
+        self.speed = self.speed + 5
+
+    def set_brake(self)
+        self.speed = self.speed - 5
+
+    def get_speed(self)
+        return speed()
