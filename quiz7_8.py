8aea86ffdf372632e9c5f51bc99233b2935d6837
Adelya <adelya@astar.app>
2022-01-05 18:27:18 -0500

    Add files via upload

---
+++ quiz7_8.py
@@ -0,0 +1,13 @@
+#Assume that the variable  names  references a list of strings. Write
+#code that determines whether 'James' is in the names list. If it is,
+#display the message, 'Hello James', Otherwise display the message
+#' No James'
+
+#Start with this
+
+names =['Eric', 'Shavvon','Kamenka' ]
+
+if 'James' in names:
+    print('Hello James')
+else:
+    print('No James')
