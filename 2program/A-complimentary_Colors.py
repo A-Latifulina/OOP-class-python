df7953a374b1e7fc79bea8260f0ee20f63e48a68
Adelya <adelya@astar.app>
2022-01-08 14:36:23 -0500

    Add files via upload

---
+++ 2program/A-complimentary_Colors.py
@@ -0,0 +1,19 @@
+#Creative extra credit
+#Enter color to find out complimentary color
+
+color = input('Enter a primary or secondary color to find out its complimentary color ')
+
+if color == 'red':
+    print('The complimentary color to', color, 'is green.')
+elif color == 'blue':
+    print('The complimentary color to', color, 'is orange.')
+elif color == 'yellow':
+    print('The complimentary color to', color, 'is purple.')
+elif color == 'green':
+    print('The complimentary color to', color, 'is red.')
+elif color == 'purple':
+    print('The complimentary color to', color, 'is yellow.')
+elif color == 'orange':
+    print('The complimentary color to', color, 'is blue.')
+else:
+    print('Error message: Make sure you entered either a primary or secondary color')
