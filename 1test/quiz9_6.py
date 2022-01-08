63e64efd7cf8c03514a3bac17c7be21f8e448081
Adelya <adelya@astar.app>
2022-01-07 22:14:44 -0500

    Update quiz9_6.py

--- 1test/quiz9_6.py
+++ 1test/quiz9_6.py
@@ -1,4 +1,3 @@
 courses_room = {}
 courses_room = {'Course':'Room number', 'CIS101':'Goldstein300', 'Mat130':'Willcox30', 'CS122':'Willcox34', 'Eng101':'Miller 35', 'Chem101':'Dyson22'}
-
 print(courses_room)
