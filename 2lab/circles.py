e60631d50c5964c17672364801eefae5360f1c98
Adelya <adelya@astar.app>
2022-01-08 14:33:39 -0500

    Create circles.py

---
+++ 2lab/circles.py
@@ -0,0 +1,10 @@
+# Working with circles
+# Area calculation
+radius = float(input('What is the radius of your circle '))
+
+area = 3.14 * radius**2
+print('The area is', area)  
+
+# Circumference calculation
+circumference = 2 * 3.14 * radius
+print('The circumference is', circumference)
