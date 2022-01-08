51d07ddaee023f01a1065a719b0f41d750f2cbf3
Adelya <adelya@astar.app>
2022-01-08 14:28:35 -0500

    Create divide.py

---
+++ 1lab/divide.py
@@ -0,0 +1,11 @@
+#dividing integers
+first = int (input('What is your first integer '))
+second = int ( ('What is your second integer '))
+
+quotient =int (input ('State the quotient of ' + str(first) + ' divided by ' + str( second)  ))
+print(you said the quotient  is  , quotient)
+if  quotient== first / second:
+    print('you are correct')
+else:
+    print('you are incorrect')
+    print ( 'the quotient is ', first / second)
