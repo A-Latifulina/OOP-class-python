58343e5666cacd23280d42f63b90b8c022b5da9d
Adelya <adelya@astar.app>
2022-01-16 23:54:45 -0500

    multiplying ints

--- 01lab/multiply.py
+++ 01lab/multiply.py
@@ -4,8 +4,8 @@ second = int (input('What is your second integer '))
 print ('State the product of ' , first , ' and ' , second)
 product = int (input ())
 print('you said the product is ' , product)
-if  product== first * second:
+if  product == first * second:
     print('you are correct')
 else:
     print('you are incorrect')
-    print ( 'the product is ', first * second)
+    print ('the product is ', first * second)
