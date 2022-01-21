bbc0ad83a7f926e5ec29478655c664ef256d4aff
Adelya <adelya@astar.app>
2022-01-20 23:24:03 -0500

    dictionary loop

--- 03program/rainfallDict.py
+++ 03program/rainfallDict.py
@@ -2,8 +2,8 @@ annual_rainfall = {}
 
 i = 1
 for i in range(2):
-    month = int(input('Enter month number '))
-    rainfall = int(input('Enter monthly rainfall '))
+    month = int(input('Enter month number: '))
+    rainfall = int(input('Enter monthly rainfall: '))
     annual_rainfall[month] = rainfall
     i += 1
 
