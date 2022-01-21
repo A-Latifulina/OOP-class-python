6b5a711f3af700e3ed9da7f1cebf09b5b83c4af8
Adelya <adelya@astar.app>
2022-01-21 16:55:06 -0500

    list collect

--- 05lab/salesList.py
+++ 05lab/salesList.py
@@ -1,7 +1,6 @@
-##sales_list.py  We are gathering dales data for  5 days
-num_days = int(input('Enter number of days ')) #Task 11
+num_days = int(input('Enter number of days '))
 
-sales= [0] * num_days #Task 11
+sales= [0] * num_days
 print(sales)
 
 i = 0
@@ -12,14 +11,14 @@ while  i < num_days:
 
 print(sales)
 
-list_num = int(input('Enter which list position to upgrade ')) #Task 12
-new_value = float(input('Enter what value to upgrade ')) #Task 12
-sales[list_num] = new_value #Task 12
+list_num = int(input('Enter which list position to upgrade '))
+new_value = float(input('Enter what value to upgrade '))
+sales[list_num] = new_value
 
-print(sales) #Task 12
+print(sales)
 
-print('The sum is', sum(sales)) #Task 13
+print('The sum is', sum(sales))
 
-print('The average is', (sum(sales)/len(sales))) #Task 13
+print('The average is', (sum(sales)/len(sales)))
 
-print('The reversed list of sales is', list(reversed(sales))) #Task 14
+print('The reversed list of sales is', list(reversed(sales)))
