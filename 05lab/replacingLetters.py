ac7283a5a3b7b2d77b05856e6437e41111b4b808
Adelya <adelya@astar.app>
2022-01-21 16:54:14 -0500

    string replace

--- 05lab/replacingLetters.py
+++ 05lab/replacingLetters.py
@@ -1,23 +1,18 @@
-# working with strings
-#This program attempts to replace every T with a '_'
-
 count = 0
 my_string = input ('Enter a sentence ')
 print('The original sentence is ', my_string) 
 
-your_string = '' #(1)
+your_string = ''
 
 i = 0
 for i in range (len(my_string)):
       ch = my_string[i]
 
       if ch =='T' or ch =='t':
-            ch = '_'#(2)
+            ch = '_'
             i = i + 1
           
-      your_string = your_string + ch #(3)
-      print(your_string) #Task 9
+      your_string = your_string + ch
+      print(your_string)
     
-
 print('The sentence with "T" or "t" replaced with "_" is', your_string)
-       
