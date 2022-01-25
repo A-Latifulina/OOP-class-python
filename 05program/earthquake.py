2e94323641f37dfcf265d63575fe2cb959fbe81d
Adelya <adelya@astar.app>
2022-01-24 20:46:13 -0500

    list, function, for, in

--- 05program/earthquake.py
+++ 05program/earthquake.py
@@ -16,10 +16,10 @@ def average_magnitude():
     for i in range (len(magnitude)):
         sum = sum + float(magnitude[i])
         average = sum / len(magnitude)
-    print('The average magnitude is', average)
+    print('The average magnitude is ', average)
 
 #main
 average_magnitude()
 
-print ('The max magnitude was', max(magnitude))
+print ('The max magnitude was ', max(magnitude))
 print ('The list of sorted locations is ', sorted(location))
